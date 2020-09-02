e58d1ca5832864fa6841de2f4bee933c4af573a5
ffont <frederic.font@gmail.com>
2020-09-02 19:25:08 +0200

    Configure ports and ips from command line

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
 
+import argparse
+
 from flask import Flask, render_template, request, redirect, url_for
 from oscpy.client import OSCClient
 
 app = Flask(__name__, static_url_path='/static', static_folder='static/')
-osc_client = OSCClient("127.0.0.1", 9000, encoding='utf8')
+osc_client = None
 
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -16,4 +18,12 @@ def index():
     return render_template("index.html")
     
 if __name__ == '__main__':
-    app.run(port=8123, debug=True)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1", help="The ip to send OSC to")
+    parser.add_argument("--osc_port", type=int, default=9000, help="The port to send OSC messages to")
+    parser.add_argument("--http_port", type=int, default=8123, help="The port the web server should listen at")
+    args = parser.parse_args()
+
+    osc_client = OSCClient(args.ip, args.osc_port, encoding='utf8')
+    app.run(port=args.http_port, debug=True)
