8c2d09c368798a3ed17987ec6f550fa82072e7b3
ffont <frederic.font@gmail.com>
2020-09-02 10:18:41 +0200

    Update README with instructions for building and notes about JUCE version

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -5,15 +5,19 @@ from fabric import task, Connection
 host = 'mind@elk-pi.local'
 remote_dir = '/home/mind/'
 
+
 @task
 def clean(ctx):
 
     # Remove all intermediate build files (for ELK build)
     os.system("rm -r ../SourceSampler/Builds/ELKAudioOS/build/")
 
+    # Remove all intermediate build files (for macOS build)
+    os.system("rm -r ../SourceSampler/Builds/MacOSX/build/")
+
 
 @task
-def send(ctx):
+def send_elk(ctx):
 
     # Copy compiled file and sushi configuration to board
     print('\nSending compiled SourceSamler and config files to board...')
@@ -28,24 +32,41 @@ def send(ctx):
             c.put(local_file, destination_dir)
 
     print('\nAll done!')
-    print('You can now run OctopushOS on the ELK board with the command:')
+    print('You can now run Source on the ELK board with the command:')
     print('./start.sh')
     print('\n')
 
 
 @task
-def compile(ctx):
+def compile_elk(ctx):
 
-    # Cross-compile OctopushOS
-    print('Coss-compiling SourceSampler for ELK platform...')
+    # Cross-compile Source
+    print('Coss-compiling Source for ELK platform...')
     print('*********************************************\n')
     os.system("find ../SourceSampler/Builds/ELKAudioOS/build/intermediate/Release/ -type f \( \! -name 'include_*' \) -exec rm {} \;")
-    os.system('docker run --rm -it -v elkvolume:/workdir -v ${PWD}/../:/code/source -v ${PWD}/../../JUCE_ELK:/home/sdkuser/JUCE -v ${PWD}/../../VST_SDK/VST2_SDK:/code/VST2_SDK -v ${PWD}/custom-esdk-launch.py:/usr/bin/esdk-launch.py crops/extsdk-container')
+    os.system('docker run --rm -it -v elkvolume:/workdir -v ${PWD}/../:/code/source -v ${PWD}/../SourceSampler/3rdParty/JUCE_ELK:/home/sdkuser/JUCE -v ${PWD}/../../VST_SDK/VST2_SDK:/code/VST2_SDK -v ${PWD}/custom-esdk-launch.py:/usr/bin/esdk-launch.py crops/extsdk-container')
 
     print('\nAll done!')
 
 
 @task
-def deploy(ctx):
-    compile(ctx)
-    send(ctx)
+def compile_macos(ctx):
+
+    # Compile release Source for macOS platform
+    print('Compiling Source for macOS platform...')
+    print('*********************************************\n')
+    os.system("cd ../SourceSampler/Builds/MacOSX/;xcodebuild -configuration Release GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS JUCER_ENABLE_GPL_MODE=1' LLVM_LTO=NO")    
+
+    print('\nAll done!')
+
+
+@task
+def compile(ctx):
+    compile_macos(ctx)
+    compile_elk(ctx)
+    
+    
+@task
+def deploy_elk(ctx):
+    compile_elk(ctx)
+    send_elk(ctx)
