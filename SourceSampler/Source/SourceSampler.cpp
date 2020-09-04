005447fb902b69bcadc5e24f268f2506d77d29ee
ffont <frederic.font@gmail.com>
2020-09-04 08:05:19 +0200

    Move SourceSamplerSound and SourceSamplerVoice implementation in single file

--- SourceSampler/Source/SourceSampler.cpp
+++ SourceSampler/Source/SourceSampler.cpp
@@ -11,6 +11,53 @@
 #include "SourceSampler.h"
 
 
+// --------- implementation of SourceSamplerSound
+
+
+SourceSamplerSound::SourceSamplerSound (const String& soundName,
+                                        AudioFormatReader& source,
+                                        const BigInteger& notes,
+                                        int midiNoteForNormalPitch,
+                                        double attackTimeSecs,
+                                        double releaseTimeSecs,
+                                        double maxSampleLengthSeconds)
+    : name (soundName),
+      sourceSampleRate (source.sampleRate),
+      midiNotes (notes),
+      midiRootNote (midiNoteForNormalPitch)
+{
+    if (sourceSampleRate > 0 && source.lengthInSamples > 0)
+    {
+        length = jmin ((int) source.lengthInSamples,
+                       (int) (maxSampleLengthSeconds * sourceSampleRate));
+
+        data.reset (new AudioBuffer<float> (jmin (2, (int) source.numChannels), length + 4));
+
+        source.read (data.get(), 0, length + 4, 0, true, true);
+
+        params.attack  = static_cast<float> (attackTimeSecs);
+        params.release = static_cast<float> (releaseTimeSecs);
+    }
+}
+
+SourceSamplerSound::~SourceSamplerSound()
+{
+}
+
+bool SourceSamplerSound::appliesToNote (int midiNoteNumber)
+{
+    return midiNotes[midiNoteNumber];
+}
+
+bool SourceSamplerSound::appliesToChannel (int /*midiChannel*/)
+{
+    return true;
+}
+
+
+// --------- implementation of SourceSamplerVoice
+
+
 SourceSamplerVoice::SourceSamplerVoice() {}
 SourceSamplerVoice::~SourceSamplerVoice() {}
 
