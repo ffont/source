23772d82cdcab4fc5933d7510a726d1103463233
ffont <frederic.font@gmail.com>
2020-11-10 11:25:54 +0100

    Move fabfile, update main app to better integrate ELK hw connection

--- fabfile.py
+++ fabfile.py
@@ -10,10 +10,10 @@ remote_dir = '/home/mind/'
 def clean(ctx):
 
     # Remove all intermediate build files (for ELK build)
-    os.system("rm -r ../SourceSampler/Builds/ELKAudioOS/build/")
+    os.system("rm -r SourceSampler/Builds/ELKAudioOS/build/")
 
     # Remove all intermediate build files (for macOS build)
-    os.system("rm -r ../SourceSampler/Builds/MacOSX/build/")
+    os.system("rm -r SourceSampler/Builds/MacOSX/build/")
 
 
 @task
@@ -24,17 +24,16 @@ def send_elk(ctx):
     print('********************************************************\n')
     with Connection(host=host, connect_kwargs={'password': 'elk'}) as c:
         for local_file, destination_dir in [
-            ("start", remote_dir),
-            ("stop", remote_dir),
-            ("source_sushi_config.json", remote_dir),
-            ("main", remote_dir),
-            ("requirements.txt", remote_dir),
-            ("LiberationMono-Regular.ttf", remote_dir),
-            ("elk_ui.py", remote_dir),
-            ("test_glue_app.py", remote_dir),
-            ("source_sensei_config.json", remote_dir),
-            ("../SourceSampler/Resources/index.html", remote_dir),
-            ("../SourceSampler/Builds/ELKAudioOS/build/SourceSampler.so", remote_dir)
+            ("elk_platform/start", remote_dir),
+            ("elk_platform/stop", remote_dir),
+            ("elk_platform/source_sushi_config.json", remote_dir),
+            ("elk_platform/main", remote_dir),
+            ("elk_platform/requirements.txt", remote_dir),
+            ("elk_platform/LiberationMono-Regular.ttf", remote_dir),
+            ("elk_platform/elk_ui.py", remote_dir),
+            ("elk_platform/source_sensei_config.json", remote_dir),
+            ("SourceSampler/Resources/index.html", remote_dir),
+            ("SourceSampler/Builds/ELKAudioOS/build/SourceSampler.so", remote_dir)
         ]:
             print('- Copying {0} to {1}'.format(local_file, destination_dir))
             c.put(local_file, destination_dir)
@@ -53,7 +52,7 @@ def compile_elk(ctx):
 
     # Generate binary files
     print('\n* Building BinaryData')
-    os.system("../SourceSampler/3rdParty/JUCE/extras/BinaryBuilder/Builds/MacOSX/build/Debug/BinaryBuilder ../SourceSampler/Resources ../SourceSampler/Source/ BinaryData")
+    os.system("SourceSampler/3rdParty/JUCE/extras/BinaryBuilder/Builds/MacOSX/build/Debug/BinaryBuilder SourceSampler/Resources SourceSampler/Source/ BinaryData")
 
     # When compiling in ELK, the JUCE_WEB_BROWSER option of juce_gui_extra needs to be disabled, but we do want it to be
     # enabled when compiling the plugin for other platforms. The solution we adopt is that here we live modify the source files
@@ -70,33 +69,33 @@ def compile_elk(ctx):
         open(path, 'w').write(new_file_contents)
         return file_contents
 
-    old_appconfig_file_contents = replace_in_file("../SourceSampler/JuceLibraryCode/AppConfig.h", 
+    old_appconfig_file_contents = replace_in_file("SourceSampler/JuceLibraryCode/AppConfig.h", 
         text_to_replace="#define   JUCE_WEB_BROWSER 1", 
         replacement="#define   JUCE_WEB_BROWSER 0")
 
-    old_makefile_file_contents = replace_in_file("../SourceSampler/Builds/ELKAudioOS/Makefile", 
+    old_makefile_file_contents = replace_in_file("SourceSampler/Builds/ELKAudioOS/Makefile", 
         text_to_replace="@pkg-config --print-errors alsa freetype2 webkit2gtk-4.0 gtk+-x11-3.0 libcurl", 
         replacement="@pkg-config --print-errors alsa freetype2 libcurl")
 
-    replace_in_file("../SourceSampler/Builds/ELKAudioOS/Makefile", 
+    replace_in_file("SourceSampler/Builds/ELKAudioOS/Makefile", 
         text_to_replace="JUCE_LDFLAGS += $(TARGET_ARCH) -L$(JUCE_BINDIR) -L$(JUCE_LIBDIR) -L/workdir/sysroots/aarch64-elk-linux/usr/lib/ $(shell pkg-config --libs alsa freetype2 webkit2gtk-4.0 gtk+-x11-3.0 libcurl) -fvisibility=hidden -lrt -ldl -lpthread $(LDFLAGS)", 
         replacement="JUCE_LDFLAGS += $(TARGET_ARCH) -L$(JUCE_BINDIR) -L$(JUCE_LIBDIR) -L/workdir/sysroots/aarch64-elk-linux/usr/lib/ $(shell pkg-config --libs alsa freetype2 libcurl) -fvisibility=hidden -lrt -ldl -lpthread $(LDFLAGS)")
     
-    replace_in_file("../SourceSampler/Builds/ELKAudioOS/Makefile", 
+    replace_in_file("SourceSampler/Builds/ELKAudioOS/Makefile", 
         text_to_replace="JUCE_CPPFLAGS := $(DEPFLAGS) -DLINUX=1 -DNDEBUG=1 -DJUCER_LINUX_MAKE_84E12380=1 -DJUCE_APP_VERSION=0.0.1 -DJUCE_APP_VERSION_HEX=0x1 $(shell pkg-config --cflags alsa freetype2 webkit2gtk-4.0 gtk+-x11-3.0 libcurl) -pthread -I$(HOME)/JUCE/modules/juce_audio_processors/format_types/VST3_SDK -I/code/VST2_SDK -I../../JuceLibraryCode -I$(HOME)/JUCE/modules -I/workdir/sysroots/aarch64-elk-linux/usr/include/freetype2/ -I../../3rdParty/cpp-httplib/ -I../../3rdParty/ff_meters/LevelMeter/ $(CPPFLAGS)", 
         replacement="JUCE_CPPFLAGS := $(DEPFLAGS) -DLINUX=1 -DNDEBUG=1 -DJUCER_LINUX_MAKE_84E12380=1 -DJUCE_APP_VERSION=0.0.1 -DJUCE_APP_VERSION_HEX=0x1 $(shell pkg-config --cflags alsa freetype2 libcurl) -pthread -I$(HOME)/JUCE/modules/juce_audio_processors/format_types/VST3_SDK -I/code/VST2_SDK -I../../JuceLibraryCode -I$(HOME)/JUCE/modules -I/workdir/sysroots/aarch64-elk-linux/usr/include/freetype2/ -I../../3rdParty/cpp-httplib/ -I../../3rdParty/ff_meters/LevelMeter/ $(CPPFLAGS)")
     
 
     # Cross-compile Source
     print('\n* Cross-compiling')
-    os.system("find ../SourceSampler/Builds/ELKAudioOS/build/intermediate/Release/ -type f \( \! -name 'include_*' \) -exec rm {} \;")
-    os.system('docker run --rm -it -v elkvolume:/workdir -v ${PWD}/../:/code/source -v ${PWD}/../SourceSampler/3rdParty/JUCE_ELK:/home/sdkuser/JUCE -v ${PWD}/../../VST_SDK/VST2_SDK:/code/VST2_SDK -v ${PWD}/custom-esdk-launch.py:/usr/bin/esdk-launch.py crops/extsdk-container')
+    os.system("find SourceSampler/Builds/ELKAudioOS/build/intermediate/Release/ -type f \( \! -name 'include_*' \) -exec rm {} \;")
+    os.system('docker run --rm -it -v elkvolume:/workdir -v ${PWD}/:/code/source -v ${PWD}/SourceSampler/3rdParty/JUCE_ELK:/home/sdkuser/JUCE -v ${PWD}/../VST_SDK/VST2_SDK:/code/VST2_SDK -v ${PWD}/elk_platform/custom-esdk-launch.py:/usr/bin/esdk-launch.py crops/extsdk-container')
 
 
     # Undo file replacements
     print('\n* Restoring build files')
-    replace_in_file("../SourceSampler/JuceLibraryCode/AppConfig.h", replacement=old_appconfig_file_contents)
-    replace_in_file("../SourceSampler/Builds/ELKAudioOS/Makefile", replacement=old_makefile_file_contents)
+    replace_in_file("SourceSampler/JuceLibraryCode/AppConfig.h", replacement=old_appconfig_file_contents)
+    replace_in_file("SourceSampler/Builds/ELKAudioOS/Makefile", replacement=old_makefile_file_contents)
     
 
     print('\nAll done!')
@@ -108,7 +107,7 @@ def compile_macos(ctx):
     # Compile release Source for macOS platform
     print('Compiling Source for macOS platform...')
     print('*********************************************\n')
-    os.system("cd ../SourceSampler/Builds/MacOSX/;xcodebuild -configuration Release GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS JUCER_ENABLE_GPL_MODE=1' LLVM_LTO=NO")    
+    os.system("cd SourceSampler/Builds/MacOSX/;xcodebuild -configuration Release GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS JUCER_ENABLE_GPL_MODE=1' LLVM_LTO=NO")    
 
     print('\nAll done!')
 
@@ -119,7 +118,7 @@ def compile_projucer_macos(ctx):
     # Compile the projucer version compatible with source
     print('Compiling Projucer for macOS platform...')
     print('*********************************************\n')
-    os.system("cd ../SourceSampler/3rdParty/JUCE_ELK/extras/Projucer/Builds/MacOSX/;xcodebuild -configuration Release GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS JUCER_ENABLE_GPL_MODE=1' LLVM_LTO=NO")    
+    os.system("cd SourceSampler/3rdParty/JUCE_ELK/extras/Projucer/Builds/MacOSX/;xcodebuild -configuration Release GCC_PREPROCESSOR_DEFINITIONS='$GCC_PREPROCESSOR_DEFINITIONS JUCER_ENABLE_GPL_MODE=1' LLVM_LTO=NO")    
 
     print('\nAll done!')
 
