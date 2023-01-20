85697943feb1fc52349de241cc04b36c732bc8d3
seatabay <seatabay@gmail.com>
2023-01-20 19:09:52 +0100

    Add config to include state synch in bundling

--- SourceSampler/3rdParty/StaticBundle/src/index.js
+++ SourceSampler/3rdParty/StaticBundle/src/index.js
@@ -1,10 +1,14 @@
 const $ = require('jquery');
 Window.prototype.$ = $;
-require('wavesurfer');
-require('adsr-envelope');
+const WaveSurfer = require('wavesurfer');
+Window.prototype.WaveSurfer = WaveSurfer;
+const ADSREnvelope = require('adsr-envelope');
+Window.prototype.ADSREnvelope  = ADSREnvelope;
 require('jquery-knob');
 const noUiSlider = require('nouislider');
 Window.prototype.noUiSlider = noUiSlider;
 const wNumb = require('wnumb');
 Window.prototype.wNumb = wNumb;
 import styles from './style.css'
+import StateSynchronizer from './core';
+export {StateSynchronizer};
--- SourceSampler/3rdParty/StaticBundle/webpack.config.js
+++ SourceSampler/3rdParty/StaticBundle/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry:'./src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    library: 'Core',
   },
   module: {
     rules: [
